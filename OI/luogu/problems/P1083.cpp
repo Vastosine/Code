ff610f58ab06728eff31b07b7b383d0c1913b3d2
Vastosine <2261819266@qq.com>
2026-09-01 17:30:22 +0800

    luogu algorithm #200

--- OI/luogu/problems/P1083.cpp
+++ OI/luogu/problems/P1083.cpp
@@ -17,44 +17,66 @@ template<typename T> std::istream &operator>>(std::istream &in, vector<T> &x) { 
 template<typename T> void sort(vector<T> &a) { std::sort(a.begin(), a.end()); }
 template<typename T, typename C> void sort(vector<T> &a, C cmp) { std::sort(a.begin(), a.end(), cmp); }
 
+using std::min;
+const static int INF = 0x7fffffff;
+
+struct SegTree {
+#define ls (k << 1)
+#define rs (ls | 1)
+#define mid ((l + r) >> 1)
+#define Ls ls, l, mid
+#define Rs rs, mid + 1 , r
+#define lm (mid - l + 1)
+#define rm (r - mid)
+#define nm (r - l + 1)
+    vi a, b;
+    int n;
+    SegTree(int n) : n(n), b(n << 2), a(n << 2, INF) {}
+
+    SegTree(const vi &data) : n(data.size() - 1) {
+        a.assign(n << 2, INF);
+        b.assign(n << 2, 0);
+        build(1, 1, n, data);
+    }
+
+    void build(int k, int l, int r, const vi &data) {
+        if (l == r) return a[k] = data[l], void();
+        build(Ls, data);
+        build(Rs, data);
+        a[k] = min(a[ls], a[rs]);
+    }
+
+    bool dim(int k, int l, int r, int L, int R, int x) {
+        if (l > R || L > r) return true;
+        if (l >= L && r <= R) return b[k] += x, (a[k] -= x) >= 0;
+        a[ls] -= b[k];
+        a[rs] -= b[k];
+        b[ls] += b[k];
+        b[rs] += b[k];
+        b[k] = 0;
+        dim(Ls, L, R, x);
+        dim(Rs, L, R, x);
+        return (a[k] = min(a[ls], a[rs])) >= 0;
+    }
+};
+
 void solve() {
     int n, m;
     cin >> n >> m;
-    vi a(n + 2), b, p, q;
-    b = p = q = vi(m + 1);
+    vi a(n + 1);
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    for (int i = n; i; i--) {
-        a[i] -= a[i - 1];
-    }
+    SegTree s(a);
     for (int i = 1; i <= m; i++) {
-        cin >> b[i] >> p[i] >> q[i];
-    }
-    int id = 0, l = 0, r = m + 1;
-    while (r - l > 1) {
-        int w = (l + r) / 2;
-        int k = w > id ? 1 : -1;
-        while (id != w) {
-            if (w > id) id++;
-            a[p[id]] -= k * b[id];
-            a[q[id] + 1] += k * b[id];
-            if (w < id) id--;
+        int d, l, r;
+        cin >> d >> l >> r;
+        if (!s.dim(1, 1, n, l, r, d)) {
+            cout << "-1\n" << i;
+            return;
         }
-        int y = 0;
-        bool ans = true;
-        for (int i = 1; i <= n; i++) {
-            y += a[i];
-            if (y < 0) {
-                ans = false;
-                break;
-            }
-        }
-        if (ans) l = w;
-        else r = w;
     }
-    if (r == m + 1) cout << 0;
-    else cout << "-1\n" << r;
+    cout << 0;
 }
 
 #undef int
