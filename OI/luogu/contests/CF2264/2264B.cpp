90d5edab77c0fcec16d9563f6644cd834b7b7109
Vastosine <2261819266@qq.com>
2026-09-21 10:36:56 +0800

    CF2264 empty

---
+++ OI/luogu/contests/CF2264/2264B.cpp
@@ -0,0 +1,32 @@
+// Problem : B. Knife's Pill Farm https://codeforces.com/contest/2264/problem/B
+// Time    : 2026-09-21 10:28:34
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// #define int long long
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::string;
+typedef vector<int> vi;
+typedef std::pair<int, int> pii;
+template<typename T> std::istream &operator>>(std::istream &in, vector<T> &x) { for (T &i : x) in >> i; return in; }
+template<typename T> void sort(vector<T> &a) { std::sort(a.begin(), a.end()); }
+template<typename T, typename C> void sort(vector<T> &a, C cmp) { std::sort(a.begin(), a.end(), cmp); }
+
+void solve() {
+
+}
+
+#undef int
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int c = 1;
+    // cin >> c;
+    while (c--) solve();
+    return 0;
+}
